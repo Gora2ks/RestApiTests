bb70c14f7d806f348691b453fa5a9cd3c0585996
Gora2ks <gora2ks@gmail.com>
2020-01-28 14:26:16 +0100

    add file all library.bat

--- TestCases/db_engine_go.py
+++ TestCases/db_engine_go.py
@@ -1,7 +1,7 @@
 from sqlalchemy import create_engine, MetaData
 import sqlite3
 
-engine = create_engine('sqlite: D:/Develop/Python_source/10Clouds_task/db.sqlite3', convert_unicode=True)
+engine = create_engine('sqlite: D:/Develop/Python_source/RestApiTests/db.sqlite3', convert_unicode=True)
 metadata = MetaData(bind=engine)
 
 engine.execute("select * from pin")
